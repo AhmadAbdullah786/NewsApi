74efbabe7ec476cbfa2585f8deb772066dc5dc49
sheikhahmad799@gmail.com <sheikhahmad799@gmail.com>
2023-10-29 17:23:11 +0500

    Unit test . of this process which task implemented

---
+++ test/NewsApi.Tests/NewsControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Moq;
+using NewsApi.Controllers;
+using NewsApi.Models;
+using Xunit;
+
+namespace NewsApi.Tests
+{
+    public class NewsControllerTests
+    {
+        [Fact]
+        public async Task GetRecentNewsShouldReturnOK()
+        {
+
+            var newsServiceMock = new Mock<NewsApiService>();
+            var controller = new NewsController(newsServiceMock.Object);
+
+            var result = await controller.GetRecentNewsByCompanyNameAndInMultipleLanguage("Microsoft", "en");
+
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+    }
+}
